438505862c48addb30441952c39e802f5f1a62d6
luojinlong <luojinlong@galbot.com>
2025-12-26 16:35:33 +0800

    add

--- configs/sql/zt_day.sql
+++ configs/sql/zt_day.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `zp_jj` (
+CREATE TABLE `zt_day` (
     `id` bigint NOT NULL AUTO_INCREMENT COMMENT '自增主键',
     `stock_name` varchar(100) NOT NULL COMMENT '股票简称',
     `latest_price` decimal(10, 2) DEFAULT NULL COMMENT '最新价',
@@ -8,8 +8,6 @@ CREATE TABLE `zp_jj` (
     `morning_auction_amount_str` varchar(50) DEFAULT '' COMMENT '早盘竞价金额(万/亿)',
     `turnover_str` varchar(50) DEFAULT '' COMMENT '成交额(万/亿)',
     `circulation_market_value` decimal(20, 2) DEFAULT NULL COMMENT '流通市值',
-    `auction_unmatched_amount_rank` varchar(50) DEFAULT NULL COMMENT '竞价未匹配金额排名',
-    `auction_unmatched_amount_rank_num` int DEFAULT '0' COMMENT '竞价未匹配金额排名数字',
     `stock_code` varchar(20) NOT NULL COMMENT '股票代码（含后缀）',
     `trade_date` date NOT NULL COMMENT '交易日期',
     `market_code` varchar(10) DEFAULT NULL COMMENT '市场代码',
@@ -31,4 +29,4 @@ CREATE TABLE `zp_jj` (
     KEY `idx_code` (`code`),
     KEY `idx_stock_code` (`stock_code`),
     KEY `idx_trade_date` (`trade_date`)
-) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '竞价数据表'
+) ENGINE = InnoDB AUTO_INCREMENT = 0 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'zt数据表'
